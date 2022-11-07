943cf1f4541479fd67c4e3942622d4a303367875
ivcp <blizujemars@gmail.com>
2022-11-07 17:40:56 +0100

    coursecontents

---
+++ part2/coursecontents/src/components/Total.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((prev, curr) => prev + curr.exercises, 0);
+  return (
+    <p>
+      <strong>total of {total} exercises</strong>
+    </p>
+  );
+};
+
+export default Total;
