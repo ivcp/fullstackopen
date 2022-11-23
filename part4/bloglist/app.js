019ceeaa6d5f33494bd38b5c439955979f0506a7
ivcp <blizujemars@gmail.com>
2022-11-23 10:56:14 +0100

    4.2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const { MONGODB_URI } = require('./utils/config');
+const blogsRouter = require('./controllers/blogs');
+
+mongoose.connect(MONGODB_URI).then(() => console.log('connected to DB'));
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.use('/api/blogs', blogsRouter);
+
+module.exports = app;
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,42 +1,6 @@
-const http = require('http');
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
+const app = require('./app');
+const { PORT } = require('./utils/config');
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
-
-const mongoUrl =
-  'mongodb+srv://fullstack:favocado1988@cluster0.b8maf2h.mongodb.net/bloglist?retryWrites=true&w=majority';
-mongoose.connect(mongoUrl).then(() => console.log('connected to DB'));
-
-app.use(cors());
-app.use(express.json());
-app.use(morgan('dev'));
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs);
-  });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then(result => {
-    response.status(201).json(result);
-  });
-});
-
-const PORT = 3003;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
