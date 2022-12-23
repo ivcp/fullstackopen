ae8508eabcd2306b2ddfeae4cbe1671e64e50b4d
ivica <blizujemars@gmail.com>
2022-12-23 12:51:46 +0100

    deployed backend to fly.io

--- part7/bloglist/bloglist - backend/index.js
+++ part7/bloglist/bloglist - backend/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 const { PORT } = require('./utils/config');
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT || 8080, () => {
+  console.log(`Server running on port ${PORT || 8080}`);
 });
