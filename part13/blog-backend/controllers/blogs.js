150f40d31839216c344a4b8a87ccb5935971bcf2
ivica <blizujemars@gmail.com>
2023-03-12 17:49:03 +0100

    13.5-13.7

---
+++ part13/blog-backend/controllers/blogs.js
@@ -0,0 +1,47 @@
+const router = require('express').Router();
+const { Blog } = require('../models');
+
+const blogFinder = async (req, _res, next) => {
+  req.blog = await Blog.findByPk(req.params.id);
+  next();
+};
+
+router.get('/', async (_req, res) => {
+  const blogs = await Blog.findAll();
+  return res.json(blogs);
+});
+
+router.get('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    return res.json(req.blog);
+  } else {
+    return res.status(404).end();
+  }
+});
+
+router.post('/', async (req, res) => {
+  const newBlog = await Blog.create(req.body);
+  return res.json(newBlog);
+});
+
+router.delete('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    await req.blog.destroy();
+  }
+  res.status(204).end();
+});
+
+router.put('/:id', blogFinder, async (req, res) => {
+  if (!req.body.likes) {
+    return res.status(400).end();
+  }
+
+  if (req.blog) {
+    await req.blog.update({ likes: req.body.likes });
+    return res.status(201).send(`updated likes to ${req.body.likes}`);
+  } else {
+    return response.status(400).end();
+  }
+});
+
+module.exports = router;
--- part13/blog-backend/index.js
+++ part13/blog-backend/index.js
@@ -1,88 +1,25 @@
-require('dotenv').config();
-const { Sequelize, QueryTypes, Model, DataTypes } = require('sequelize');
+require('express-async-errors');
+const { PORT } = require('./util/config');
+const { connectToDatabase } = require('./util/db');
+const blogsRouter = require('./controllers/blogs');
 const cors = require('cors');
 const express = require('express');
+const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+app.use('/api/blogs', blogsRouter);
 
-class Blog extends Model {}
+app.use(errorHandler);
 
-Blog.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    author: {
-      type: DataTypes.STRING,
-    },
-    url: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    likes: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-    },
-  },
-  {
-    sequelize,
-    underscored: true,
-    timestamps: false,
-    modelName: 'blog',
-  }
-);
+const start = async () => {
+  await connectToDatabase();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
 
-(async () => await Blog.sync())();
-
-app.get('/api/blogs', async (_req, res) => {
-  try {
-    const blogs = await Blog.findAll();
-    return res.json(blogs);
-  } catch (error) {
-    return res.status(400).send({ error });
-  }
-});
-
-app.get('/api/blogs/:id', async (req, res) => {
-  const blog = await Blog.findByPk(req.params.id);
-  if (blog) {
-    return res.json(blog);
-  } else {
-    return res.status(404).end();
-  }
-});
-
-app.post('/api/blogs/', async (req, res) => {
-  try {
-    const newBlog = await Blog.create(req.body);
-    return res.json(newBlog);
-  } catch (error) {
-    return res.status(400).send({ error });
-  }
-});
-
-app.delete('/api/blogs/:id', async (req, res) => {
-  const blogToDelete = await Blog.findByPk(req.params.id);
-  try {
-    await blogToDelete.destroy();
-    res.status(200).end();
-  } catch (error) {
-    res.status(400).end();
-  }
-});
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+start();
