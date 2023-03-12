cecc5d4bacc279126cffca8dbbb4d1c8af3b8e77
ivica <blizujemars@gmail.com>
2023-03-12 15:56:13 +0100

    13.4

--- part13/blog-backend/index.js
+++ part13/blog-backend/index.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 const { Sequelize, QueryTypes, Model, DataTypes } = require('sequelize');
+const cors = require('cors');
+const express = require('express');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 const sequelize = new Sequelize(process.env.DATABASE_URL);
 
@@ -36,17 +43,46 @@ Blog.init(
   }
 );
 
-const main = async () => {
+(async () => await Blog.sync())();
+
+app.get('/api/blogs', async (_req, res) => {
   try {
     const blogs = await Blog.findAll();
-    blogs.forEach(blog =>
-      console.log(
-        `${blog.dataValues.author}: ${blog.dataValues.title} ${blog.dataValues.likes} likes`
-      )
-    );
+    return res.json(blogs);
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    return res.status(400).send({ error });
   }
-};
+});
 
-main();
+app.get('/api/blogs/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id);
+  if (blog) {
+    return res.json(blog);
+  } else {
+    return res.status(404).end();
+  }
+});
+
+app.post('/api/blogs/', async (req, res) => {
+  try {
+    const newBlog = await Blog.create(req.body);
+    return res.json(newBlog);
+  } catch (error) {
+    return res.status(400).send({ error });
+  }
+});
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  const blogToDelete = await Blog.findByPk(req.params.id);
+  try {
+    await blogToDelete.destroy();
+    res.status(200).end();
+  } catch (error) {
+    res.status(400).end();
+  }
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
